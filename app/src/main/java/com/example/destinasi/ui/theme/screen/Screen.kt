5e652f2f29fea687f560d671c7364007a8fc7e4a
Sandhi Rizki <sandhirizki1102@gmail.com>
2025-05-11 21:12:20 +0700

    Mmebuat Route antar Screen

---
+++ app/src/main/java/com/example/destinasi/ui/theme/screen/Screen.kt
@@ -0,0 +1,15 @@
+package com.example.destinasi.ui.theme.screen
+
+sealed class Screen(val route: String) {
+    object Main : Screen("main_screen")
+    object Detail : Screen("detail_screen") {
+        fun withArgs(destinasiId: Long?): String {
+            return if (destinasiId != null) {
+                "detail_screen?destinasiId=$destinasiId"
+            } else {
+                "detail_screen"
+            }
+        }
+    }
+    // Tambahkan screen lain jika ada
+}
